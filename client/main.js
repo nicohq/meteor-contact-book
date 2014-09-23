319b361244a94c221c777c95e73223fbd788f9cb
Nico <nico_t@ukr.net>
2014-09-23 11:11:30 +0300

    Add accounts-entry & gavatar util

--- client/main.js
+++ client/main.js
@@ -1,6 +1,15 @@
 // Contacts subscribe
 // Meteor.subscribe('contacts'); ==> Move in lib/router.js
 
-Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_ONLY'
+
+// Auth settings
+//
+
+Meteor.startup(function() {
+    AccountsEntry.config({
+        passwordSignupFields: 'EMAIL_ONLY',
+        wrapLinks: true, // wrap links in li tag
+        homeRoute: '/sign-in', // MUST BE SET - redirect to this path after sign-out
+        dashboardRoute: '/' // MUST BE SET - redirect to this path after sign-in
+    });
 });
--- client/views/contacts/contact_item.js
+++ client/views/contacts/contact_item.js
@@ -3,5 +3,11 @@ Template.contactItem.helpers({
         var a = document.createElement('a');
         a.href = this.url;
         return a.hostname;
+    },
+    gavatar: function() {
+        // var email = 'nico_t@ukr.net';
+        // var md5 = CryptoJS.MD5(email);
+        // return 'https://www.gravatar.com/avatar/' + md5;
+        return util.getGavatar('nico_t@ukr.net', 30);
     }
 });
